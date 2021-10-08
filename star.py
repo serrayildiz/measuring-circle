9b9bc02e15b7147f392b972e4539312b60055235
serrayildiz <89711168+serrayildiz@users.noreply.github.com>
2021-10-08 23:05:06 +0300

    Add files via upload

---
+++ star.py
@@ -0,0 +1,90 @@
+from scipy.spatial import distance as dist
+from imutils import perspective
+from imutils import contours
+import argparse
+import numpy as np
+import imutils
+import cv2
+
+
+def midpoint(ptA, ptB):
+    return ((ptA[0] + ptB[0]) * 0.5, (ptA[1] + ptB[1]) * 0.5)
+
+def show_image(title, image, destroy_all=True):
+    cv2.imshow(title, image)
+    cv2.waitKey(0)
+    if destroy_all:
+        cv2.destroyAllWindows()
+
+
+image = cv2.imread("C:/Users/LENOVO/Desktop/project/srr.jpg")
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+gray = cv2.GaussianBlur(gray, (7, 7), 0)
+
+edged = cv2.Canny(gray, 50, 100)
+
+edged = cv2.dilate(edged, None, iterations=1)
+edged = cv2.erode(edged, None, iterations=1)
+
+
+cnts = cv2.findContours(edged, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+print("Total number of contours are: ", len(cnts))
+
+(cnts, _) = contours.sort_contours(cnts)
+pixelPerMetric = None
+
+
+count = 0
+for c in cnts:
+    if cv2.contourArea(c) < 100:
+        continue
+    count += 1
+
+    orig = image.copy()
+    box = cv2.minAreaRect(c)
+    box = cv2.cv.BoxPoints(box) if imutils.is_cv2() else cv2.boxPoints(box)
+    box = np.array(box, dtype="int")
+
+    box = perspective.order_points(box)
+    cv2.drawContours(orig, [box.astype("int")], -1, (0, 255, 0), 2)
+
+    for (x, y) in box:
+        cv2.circle(orig, (int(x), int(y)), 5, (0, 0, 255), -1)
+
+
+    (tl, tr, br, bl) = box
+    (tltrX, tltrY) = midpoint(tl, tr)
+    (blbrX, blbrY) = midpoint(bl, br)
+    (tlblX, tlblY) = midpoint(tl, bl)
+    (trbrX, trbrY) = midpoint(tr, br)
+
+    cv2.circle(orig, (int(tltrX), int(tltrY)), 5, (255, 0, 0), -1)
+    cv2.circle(orig, (int(blbrX), int(blbrY)), 5, (255, 0, 0), -1)
+    cv2.circle(orig, (int(tlblX), int(tlblY)), 5, (255, 0, 0), -1)
+    cv2.circle(orig, (int(trbrX), int(trbrY)), 5, (255, 0, 0), -1)
+
+    cv2.line(orig, (int(tltrX), int(tltrY)), (int(blbrX), int(blbrY)), (255, 0, 255), 2)
+    cv2.line(orig, (int(tlblX), int(tlblY)), (int(trbrX), int(trbrY)), (255, 0, 255), 2)
+
+    dA = dist.euclidean((tltrX, tltrY), (blbrX, blbrY))
+    dB = dist.euclidean((tlblX, tlblY), (trbrX, trbrY))
+
+    if pixelPerMetric is None:
+        pixelPerMetric = dB / 0.05905512
+
+    dimA = dA / pixelPerMetric *25.4
+    dimB = dB / pixelPerMetric *25.4
+
+    cv2.putText(orig, "{:.1f}mm".format(dimA), (int(tltrX - 15), int(tltrY - 10)), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (255, 255, 255), 2)
+    cv2.putText(orig, "{:.1f}mm".format(dimB), (int(trbrX + 10), int(trbrY)), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (255, 255, 255), 2)
+
+    cv2.imshow("Calculated Dimension", orig)
+
+    k = cv2.waitKey(0)
+    if k == 27:
+        break    
+
+    cv2.destroyAllWindows()
+
+print("Total contours processed: ", count)
